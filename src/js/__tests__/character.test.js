492fe2388b7e11c30237ae393a6fb273732e7cf1
nirastor <62954081+nirastor@users.noreply.github.com>
2020-10-29 22:20:13 +0300

    Delete character.test.js

--- src/js/__tests__/character.test.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-unused-vars */
-import Character from '../Character';
-
-test('Should throw error new Character not allowed', () => {
-  expect(() => { const t = new Character(1, 'test'); })
-    .toThrow('Character is basic class. new Character not allowed');
-});
